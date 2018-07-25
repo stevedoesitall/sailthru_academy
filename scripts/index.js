e85ed58fee8c9066f03c8fae52899c433f7bc836
stevedoesitall <stevegiordano@MBA0395.local>
2018-07-25 13:30:23 -0400

    Adding express scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
+//Create app using express.js
+const port = process.env.PORT || 3000;
+const express = require("express");
+const body_parser = require("body-parser");
+const path = require("path");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const dir = path.join(__dirname, "../");
+const warning = {"type" : "warning", "message" : "What is your damage, little boy? You have a sick and twisted world perspective."};
+const api_key = process.env.api_key;
+const api_secret = process.env.api_secret;
+const sailthru = require("sailthru-client").createSailthruClient(api_key, api_secret);
+
+app.use(express.static(dir));
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
+app.listen(port, () => console.log("Jon's Plex app started on port " + port));
+
+
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-const api_key = process.env.api_key;
-const api_secret = process.env.api_secret;
-const sailthru = require('sailthru-client').createSailthruClient(api_key, api_secret);
+
 const all_events = [];
 
 app.post("/email", function(req, res) {
